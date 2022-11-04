c9baac4d53d4b7f253294388a8937561d24ecee2
alexandretoullec <69418272+alexandretoullec@users.noreply.github.com>
2022-11-04 13:01:37 +0100

    Add files via upload

--- day50--product filter/script.js
+++ day50--product filter/script.js
@@ -22,3 +22,30 @@ for (let i = 0 ; i < btns.length ; i++){
 
     })
 }
+
+//search filter
+
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".product-details h2");
+const noResult = document.querySelector(".no-result");
+
+
+const filterProducts = (e) => {
+    const text = e.target.value.toLowerCase();
+
+    productName.forEach((product)=> {
+        const item = product.textContent;  
+        // indexOf() return -1 if it does not find any results
+        if(item.toLowerCase().indexOf(text) != -1){
+            // in order to select the grans parent in our case store product container
+            product.parentElement.parentElement.style.display = "block";
+            noResult.style.display = "none";
+        }else{
+            product.parentElement.parentElement.style.display = "none";
+            noResult.style.display = "block";
+        }
+    })
+}
+
+
+search.addEventListener("keyup",filterProducts);
