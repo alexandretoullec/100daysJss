7eaaeb07c4f81b8b47c1539476c717c221f4e72d
alexandretoullec <69418272+alexandretoullec@users.noreply.github.com>
2022-11-17 17:23:49 +0100

    Add files via upload

--- day57--passwordStrength/script.js
+++ day57--passwordStrength/script.js
@@ -29,13 +29,69 @@ function toggle() {
     }
 }
 
+//togggle Eye
 
 function toggleEye () {
-    if(eyeIcon.classList.contains("fa-eye")){
-        password.setAttribute("type", "text");
-        eyeIcon.classList.replace("fa-eye","fa-eye-slash")
+    eyeIcon.classList.toggle("fa-eye-slash")
+}
+
+
+//chech strength password
+
+function checkStrength (password) {
+    let strength = 0;
+
+    //check lower or uppercase
+    if(password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
+        strength += 1;
+        lowUpperCase.classList.replace("fa-circle", "fa-check" )
     }else{
-        password.setAttribute("type", "password");
-        eyeIcon.classList.replace("fa-eye-slash","fa-eye")
+        lowUpperCase.classList.replace("fa-check", "fa-circle" )
+    }
+
+    // check for numbers 
+    if(password.match(/([0-9])/)){
+        strength += 1;
+        number.classList.replace("fa-circle", "fa-check" )
+    }else{
+        number.classList.replace("fa-check", "fa-circle" )
+    }
+
+    // check for special char 
+    if(password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
+        strength += 1;
+        specialChar.classList.replace("fa-circle", "fa-check" )
+    }else{
+        specialChar.classList.replace("fa-check", "fa-circle" )
+    }
+
+    if(password.length > 7){
+        strength += 1;
+        eightChar.classList.replace("fa-circle", "fa-check" )
+    }else{
+        eightChar.classList.replace("fa-check", "fa-circle" )
+    }
+
+    // update progress bar
+    if (strength == 1){
+        passwordStrength.classList.remove("pb-danger", "pb-warning", "pb-primary", "pb-success");
+        passwordStrength.classList.add("pb-danger");
+        passwordStrength.style = 'width:25%';
+        
+    }else if (strength == 2){
+        passwordStrength.classList.remove("pb-danger", "pb-warning", "pb-primary", "pb-success");
+        passwordStrength.classList.add("pb-warning");
+        passwordStrength.style = 'width:50%';
+        
+    }else if (strength == 3){
+        passwordStrength.classList.remove("pb-danger", "pb-warning", "pb-primary", "pb-success");
+        passwordStrength.classList.add("pb-primary");
+        passwordStrength.style = 'width:75%';
+        
+    }else if (strength == 4){
+        passwordStrength.classList.remove("pb-danger", "pb-warning", "pb-primary", "pb-success");
+        passwordStrength.classList.add("pb-success");
+        passwordStrength.style = 'width:100%';
     }
 }
+
